aa6f4a8a77c9d10998200c485aa46b66c75b02a8
LorenzoRegalzi <lorenzo.regalzi@gmail.com>
2020-08-01 15:09:30 +0200

    database & sidebar

--- resources/templates/front/sidebar.php
+++ resources/templates/front/sidebar.php
@@ -1,1 +1,22 @@
+
+<?php 
+
+//creata la tabella nel database la andaimo a recuperare 
+$query = 'SELECT*FROM categories';                      
+
+$showcategories = mysqli_query($connection,$query);
+
+//controlli in caso di fallimento 
+if(!$showcategories){
+    die('Richiesta fallita'.mysqli_error($connection));
+}
+
+//ciclo che ci permette di visualizzare le nostre categorie
+
+while($row = mysqli_fetch_array($showcategories)){
+    echo  " <a href='#' '>{$row['name_cat']}</a>";
+}
+
+?>
+
 <h1>sono la sidebar</h1>
